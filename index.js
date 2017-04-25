d375c30b8b6137270be70b386ca677196310bfae
Ilia Tsymbal <itsymbal@exadel.com>
2017-04-26 01:03:15 +0300

    Do not use prefix for index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const body = require('./createBody');
 
 create.createDir('Main', (path) => {
   files.forEach((file) => {
-    const name = (file.name || path) + (file.postfix || '');
+    const name = (file.name || path) + (file.name !== 'index' && file.postfix || '');
     const ext = file.ext || 'ts';
 
     create.createFile(`${path}/${name}.${ext}`, body(file, path));
